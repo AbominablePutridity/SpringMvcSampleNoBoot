cb46139738e25958cc85a565133d396765c94e92
AbominablePutridity <qq594492@gmail.com>
2025-03-30 13:39:06 +0300

    Создание пустого проекта с конфигурациями (без spring boot)

---
+++ src/main/java/com/myPackage/AppConfig.java
@@ -0,0 +1,33 @@
+package com.myPackage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author maxim
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.myPackage")
+public class AppConfig {
+    
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+    
+    // Другие бины будут добавлены ниже
+}
